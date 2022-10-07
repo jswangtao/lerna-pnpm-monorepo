c69841b249865c97ae2c198c11f38094f732c47e
tao.wang <331871902@qq.com>
2022-10-07 10:40:33 +0800

    test

--- packages/ui/lib/ui.js
+++ packages/ui/lib/ui.js
@@ -2,12 +2,13 @@
  * @Author: wangtao
  * @Date: 2022-10-06 17:26:36
  * @LastEditors: 汪滔
- * @LastEditTime: 2022-10-07 10:20:12
+ * @LastEditTime: 2022-10-07 10:40:24
  * @Description: file content
  */
 "use strict";
 
 module.exports = ui;
+1 === 1;
 1 === 1;
 function ui() {
   return "Hello from ui";
