8224c3604918a2e546fc74f906e4a1b20583e4c5
tao.wang <331871902@qq.com>
2022-10-07 14:48:31 +0800

    11111

--- packages/ui/lib/ui.js
+++ packages/ui/lib/ui.js
@@ -2,7 +2,7 @@
  * @Author: wangtao
  * @Date: 2022-10-06 17:26:36
  * @LastEditors: 汪滔
- * @LastEditTime: 2022-10-07 10:40:24
+ * @LastEditTime: 2022-10-07 14:47:33
  * @Description: file content
  */
 "use strict";
